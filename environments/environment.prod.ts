cd99d13a2a54550e40a8ef5fab507edc65eda74d
lpaszkiewicz <lpaszkiewicz1@gmail.com>
2019-06-01 11:51:18 -0400

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+   firebase: {
+      apiKey: 'AIzaSyDRKrATC2oP-kit3ziUhuIwNAVR6ouPi30',
+      authDomain: 'wrdsamngfrnds.firebaseapp.com',
+      databaseURL: 'https://wrdsamngfrnds.firebaseio.com',
+      projectId: 'wrdsamngfrnds',
+      storageBucket: 'wrdsamngfrnds.appspot.com',
+      messagingSenderId: '136895856925'
+    }
+};
