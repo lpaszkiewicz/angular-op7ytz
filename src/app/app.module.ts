3dc28379b4be157a4624c1c9b47593461f558516
lpaszkiewicz <lpaszkiewicz1@gmail.com>
2019-06-01 11:49:33 -0400

    3 files modified, 34 added, 37 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,70 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponents, AppRoutes } from './app.routing';
+import * as $ from 'jquery';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ArtComponent } from './art/art.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from './../environments/environment';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { ArtListComponent } from './art-list/art-list.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { InfoComponent } from './info/info.component';
+import { FooterComponent } from './footer/footer.component';
+import { SportsComponent } from './sports/sports.component';
+import { PeopleComponent } from './people/people.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MusicComponent } from './music/music.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    ArtComponent,
+    AppNavbarComponent,
+    ArtListComponent,
+    InfoComponent,
+    FooterComponent,
+    SportsComponent,
+    PeopleComponent,
+    MoviesComponent,
+    MusicComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule,
+    RouterModule.forRoot(AppRoutes)
+  ],
+  schemas: [ NO_ERRORS_SCHEMA ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+export const routerConfig: Routes = [
+    {
+        path: 'art',
+        component: ArtComponent
+    },
+    {
+        path: 'info',
+        component: InfoComponent
+    },
+    {
+        path: '',
+        redirectTo: '/art',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/art',
+        pathMatch: 'full'
+    }
+];
